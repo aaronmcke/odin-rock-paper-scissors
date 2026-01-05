b4cae47751f753fd7197106786aa12acebc5031b
aaronmcke <aaronmck@gmail.com>
2026-01-05 14:31:52 +0000

    Finally go there, cheated at the end a little as I couldn't figure out how to get the game to reprompt and continue, but used a for loop and moved functions around.

--- script.js
+++ script.js
@@ -18,8 +18,6 @@ function getComputerChoice () {
     return 'scissors'
   }
 }
-console.log(getComputerChoice());
-// getComputerChoice();rock
 
 /* Use a prompt to recieve the humans choice. Asking them to type rock, paper or scissors.
 take in the string and store it in the function getHuman Choice */
@@ -29,47 +27,63 @@ function getHumanChoice () {
   return weapon.toLowerCase();
 }
 
-// getHumanChoice();
-
 /* Keep scores, declare 2 variables to tally computer score and human score.*/
 
-let computerScore = 0;
-let humanScore = 0;
+
 
 // A function to play a single round to determin the winner using if statements and then declaring a point for the winner inside the score variable
 
-function playRound (computerChoice, humanChoice) {
-  humanChoice.toLowerCase;
-  if (computerChoice === humanChoice) {
-    humanScore ++; computerScore++;
-    return console.log(`The computer chose ${computerChoice} and you chose ${humanChoice}! That is a tie!`);
-  } else if (computerChoice === 'rock' && humanChoice === 'scissors') {
-    computerScore ++;
-    return console.log(`The computer chose ${computerChoice} and you chose ${humanChoice}! Computer wins!`);
-  } else if (computerChoice === 'rock' && humanChoice === 'paper') {
-    humanScore ++;
-    return console.log(`The computer chose ${computerChoice} and you chose ${humanChoice}! You win!`);
-  } else if (computerChoice === 'paper' && humanChoice === 'rock') {
-    computerScore ++;
-    return console.log(`The computer chose ${computerChoice} and you chose ${humanChoice}! Computer wins!`);
-  } else if (computerChoice === 'paper' && humanChoice === 'scissors') {
-    humanScore ++;
-    return console.log(`The computer chose ${computerChoice} and you chose ${humanChoice}! You win!`);
-  } else if (computerChoice === 'scissors' && humanChoice === 'paper') {
-    computerScore ++;
-    return console.log(`The computer chose ${computerChoice} and you chose ${humanChoice}! The computer wins!`);
-  } else if (computerChoice === 'scissors' && humanChoice === 'rock') {
-    humanScore ++;
-    return console.log(`The computer chose ${computerChoice} and you chose ${humanChoice}! You win!`);
-  } else 
-    return console.log('Something went wrong, please try again! Thanks')
+
+
+function playGame () {
+  let computerScore = 0;
+  let humanScore = 0;
+
+
+  function playRound (computerChoice, humanChoice) {
+    humanChoice.toLowerCase;
+    if (computerChoice === humanChoice) {
+      humanScore ++; computerScore++;
+      return console.log(`The computer chose ${computerChoice} and you chose ${humanChoice}! That is a tie!`);
+    } else if (computerChoice === 'rock' && humanChoice === 'scissors') {
+      computerScore ++;
+      return console.log(`The computer chose ${computerChoice} and you chose ${humanChoice}! Computer wins!`);
+    } else if (computerChoice === 'rock' && humanChoice === 'paper') {
+      humanScore ++;
+      return console.log(`The computer chose ${computerChoice} and you chose ${humanChoice}! You win!`);
+    } else if (computerChoice === 'paper' && humanChoice === 'rock') {
+      computerScore ++;
+      return console.log(`The computer chose ${computerChoice} and you chose ${humanChoice}! Computer wins!`);
+    } else if (computerChoice === 'paper' && humanChoice === 'scissors') {
+      humanScore ++;
+      return console.log(`The computer chose ${computerChoice} and you chose ${humanChoice}! You win!`);
+    } else if (computerChoice === 'scissors' && humanChoice === 'paper') {
+      computerScore ++;
+      return console.log(`The computer chose ${computerChoice} and you chose ${humanChoice}! The computer wins!`);
+    } else if (computerChoice === 'scissors' && humanChoice === 'rock') {
+      humanScore ++;
+      return console.log(`The computer chose ${computerChoice} and you chose ${humanChoice}! You win!`);
+    } else 
+      return console.log('Something went wrong, please try again! Thanks')
+    }
+    
+
+
+    for (let i =0; i < 5; i++) {
+      const humanSelection = getHumanChoice();
+      const computerSelection = getComputerChoice();
+      playRound(computerSelection, humanSelection);
+      console.log(`your score ${humanScore}, Computer Score ${computerScore}`)
+    }
+     
+      if (computerScore >= 3 ) {
+      console.log(`The computer wins with ${computerScore} points, you only had ${humanScore} points! Unlucky!`)
+    } else if (humanScore >3 ) {
+      console.log(`You win with ${humanScore} points, the computer only had ${computerScore} points! Congratulations!`)
+    }
+ 
+ 
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
-playRound(computerSelection, humanSelection); 
-
-
-console.log(computerSelection, humanSelection);
-console.log(computerScore, humanScore);
+playGame();
