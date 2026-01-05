ff15b2afeb6eebf18b63070dedf3af11a9c4ecf8
aaronmcke <aaronmck@gmail.com>
2026-01-05 13:35:48 +0000

    I have a single round now working Prompt recived from human will be made lowercase so they can enter in any case. if they incorrectly spell a weapon it will return that something went wrong, that they should try again.

--- script.js
+++ script.js
@@ -19,21 +19,57 @@ function getComputerChoice () {
   }
 }
 console.log(getComputerChoice());
+// getComputerChoice();rock
 
 /* Use a prompt to recieve the humans choice. Asking them to type rock, paper or scissors.
 take in the string and store it in the function getHuman Choice */
 
 function getHumanChoice () {
   let weapon = prompt('Please enter your weapon of choice. rock, paper or scissors')
-  console.log(choice);
+  return weapon.toLowerCase();
 }
 
-getHumanChoice();
+// getHumanChoice();
 
 /* Keep scores, declare 2 variables to tally computer score and human score.*/
 
-computerScore = 0;
-humanScore = 0;
+let computerScore = 0;
+let humanScore = 0;
 
 // A function to play a single round to determin the winner using if statements and then declaring a point for the winner inside the score variable
 
+function playRound (computerChoice, humanChoice) {
+  humanChoice.toLowerCase;
+  if (computerChoice === humanChoice) {
+    humanScore ++; computerScore++;
+    return console.log(`The computer chose ${computerChoice} and you chose ${humanChoice}! That is a tie!`);
+  } else if (computerChoice === 'rock' && humanChoice === 'scissors') {
+    computerScore ++;
+    return console.log(`The computer chose ${computerChoice} and you chose ${humanChoice}! Computer wins!`);
+  } else if (computerChoice === 'rock' && humanChoice === 'paper') {
+    humanScore ++;
+    return console.log(`The computer chose ${computerChoice} and you chose ${humanChoice}! You win!`);
+  } else if (computerChoice === 'paper' && humanChoice === 'rock') {
+    computerScore ++;
+    return console.log(`The computer chose ${computerChoice} and you chose ${humanChoice}! Computer wins!`);
+  } else if (computerChoice === 'paper' && humanChoice === 'scissors') {
+    humanScore ++;
+    return console.log(`The computer chose ${computerChoice} and you chose ${humanChoice}! You win!`);
+  } else if (computerChoice === 'scissors' && humanChoice === 'paper') {
+    computerScore ++;
+    return console.log(`The computer chose ${computerChoice} and you chose ${humanChoice}! The computer wins!`);
+  } else if (computerChoice === 'scissors' && humanChoice === 'rock') {
+    humanScore ++;
+    return console.log(`The computer chose ${computerChoice} and you chose ${humanChoice}! You win!`);
+  } else 
+    return console.log('Something went wrong, please try again! Thanks')
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(computerSelection, humanSelection); 
+
+
+console.log(computerSelection, humanSelection);
+console.log(computerScore, humanScore);
